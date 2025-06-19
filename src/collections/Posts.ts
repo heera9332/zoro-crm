162aa97b1bbf810327082f975ee9964b68b9d6ba
Heera Singh <heera.madquick@gmail.com>
2025-06-20 02:10:13 +0530

    post schema update

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -28,7 +28,6 @@ export const Posts: CollectionConfig = {
       name: "content",
       type: "richText",
       label: "Post Content",
-      required: true,
       admin: {
         style: {
           minHeight: "400px",
